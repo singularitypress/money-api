e86dbebe0f65171dd6a4efc21eba5da66fd44953
singularitypress <jaypandya2k5@gmail.com>
2023-05-08 18:17:14 -0400

    update amortization func

--- utils/calculate-amortization.ts
+++ utils/calculate-amortization.ts
@@ -12,21 +12,22 @@ interface AmortizationTableRow {
  * @param principal Loan amount
  * @param interestRate Annual interest rate as a decimal
  * @param loanTerm Loan term in years
+ * @param paymentFrequency Number of payments per year (12 for monthly, 26 for biweekly)
  * @returns Array of amortization table rows
  */
 export const calculateAmortizationTable = (
   principal: number,
   interestRate: number,
-  loanTerm: number
+  loanTerm: number,
+  paymentFrequency: number
 ): AmortizationTableRow[] => {
-  const monthlyInterestRate = interestRate / 12;
-  const totalNumberOfPayments = loanTerm * 12;
-
-  const payment =
-    (principal *
-      (monthlyInterestRate *
-        Math.pow(1 + monthlyInterestRate, totalNumberOfPayments))) /
-    (Math.pow(1 + monthlyInterestRate, totalNumberOfPayments) - 1);
+  const totalNumberOfPayments = loanTerm * paymentFrequency;
+  const monthlyInterestRate = interestRate / paymentFrequency;
+  let payment = calculatePayment(
+    principal,
+    monthlyInterestRate,
+    totalNumberOfPayments
+  );
 
   let balance = principal;
   const amortizationTable: AmortizationTableRow[] = [];
@@ -48,7 +49,38 @@ export const calculateAmortizationTable = (
       payment,
     };
     amortizationTable.push(row);
+
+    if (paymentNumber % (paymentFrequency / 12) === 0) {
+      // If we've completed a year's worth of payments, update the balance and reset the payment
+      balance -= payment - principal;
+      payment = calculatePayment(
+        balance,
+        monthlyInterestRate,
+        totalNumberOfPayments - paymentNumber
+      );
+    }
   }
 
   return amortizationTable;
+};
+
+/**
+ * Calculate the payment amount for a mortgage
+ *
+ * @param principal Loan amount
+ * @param monthlyInterestRate Monthly interest rate as a decimal
+ * @param totalNumberOfPayments Total number of payments
+ * @returns Payment amount
+ */
+export const calculatePayment = (
+  principal: number,
+  monthlyInterestRate: number,
+  totalNumberOfPayments: number
+): number => {
+  return (
+    (principal *
+      (monthlyInterestRate *
+        Math.pow(1 + monthlyInterestRate, totalNumberOfPayments))) /
+    (Math.pow(1 + monthlyInterestRate, totalNumberOfPayments) - 1)
+  );
 };
